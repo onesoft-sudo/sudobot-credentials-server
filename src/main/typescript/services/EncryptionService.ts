2418383a77573de8564b99903eea098f537a09fc
Ar Rakin <rakinar2@onesoftnet.eu.org>
2025-02-05 16:50:45 +0600

    feat: support plaintext hex keys
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- src/main/typescript/services/EncryptionService.ts
+++ src/main/typescript/services/EncryptionService.ts
@@ -2,17 +2,17 @@ import Service from "../core/Service";
 import { readFile } from "fs/promises";
 
 class EncryptionService extends Service {
-    private publicKey?: Uint8Array
+    private publicKey?: Uint8Array;
     private privateKey?: Uint8Array;
 
     public override async boot() {
         const pubKeyString = await readFile(
             process.env.SBC_PUBLIC_KEY!,
-            "binary",
+            process.env.SBC_PUBLIC_KEY?.endsWith(".keyhex") ? "hex" : "binary",
         );
         const privKeyString = await readFile(
             process.env.SBC_PRIVATE_KEY!,
-            "binary",
+            process.env.SBC_PRIVATE_KEY?.endsWith(".keyhex") ? "hex" : "binary",
         );
 
         this.publicKey = new Uint8Array(Buffer.from(pubKeyString, "binary"));
